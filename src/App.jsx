9678a7ea0d608e2d1868b403ee1f4f4bb4cd255c
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-04-20 00:36:36 +0530

    style: made the background for website

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import * as styles from './App.module.scss'
 
 import moon from './assets/moon.png'
 import dvmLogo from './assets/dvmLogoName.svg'
+import bitsPilani from './assets/bitspilaninight.png'
+import stars from './assets/Stars.png'
+import gridLeft from './assets/Grid left.png'
+import gridRight from './assets/Grid right.png'
+import shadowRectangle from './assets/Shadow rect.png'
 
 import Loader from './components/Loader/Loader'
 
@@ -10,31 +15,78 @@ function App() {
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    const assets = [moon, dvmLogo]
+    const assets = [moon, dvmLogo, bitsPilani, stars, gridLeft, gridRight]
 
-    async function loadAssets() {
+    const loadAssets = async () => {
       try {
-        new Promise.all(assets.map(asset => new Promise((resolve => {
-          const img = new Image()
-          img.src = asset
-          img.onload = resolve
-          img.onerror = resolve
-        }))))
-        setIsLoading(false)
+        await Promise.all(
+          assets.map(
+            (asset) =>
+              new Promise((resolve) => {
+                const img = new Image();
+                img.src = asset;
+                img.onload = resolve;
+                img.onerror = resolve;
+              })
+          )
+        );
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error loading assets:", error);
+        setIsLoading(false);
       }
-      catch (err) {
-        console.log(err)
-        setIsLoading(false)
-      }
-    }
+    };
 
-    loadAssets()
-  }, [])
+    loadAssets();
+  }, []);
 
   return (
     <>
       {isLoading && <Loader />}
-      <h1>Loaded</h1>
+      <img
+        src={shadowRectangle}
+        alt="shadow"
+        draggable={false}
+        className={styles.shadowRect}
+      />
+      <img
+        src={gridLeft}
+        alt="left grid"
+        draggable={false}
+        className={styles.gridLeft}
+      />
+      <img
+        src={gridRight}
+        alt="right grid"
+        draggable={false}
+        className={styles.gridRight}
+      />
+      <img
+        src={stars}
+        alt="stars"
+        draggable={false}
+        className={styles.stars}
+      />
+      <img
+        src={bitsPilani}
+        alt="clocktower"
+        draggable={false}
+        className={styles.bitsPilani}
+      />
+      <img
+        src={moon}
+        alt="moon"
+        draggable={false}
+        className={styles.moon}
+      />
+      <main className={styles.page}>
+        <img
+          src={dvmLogo}
+          alt="dvm"
+          draggable={false}
+          className={styles.dvmLogo}
+        />
+      </main>
     </>
   )
 }

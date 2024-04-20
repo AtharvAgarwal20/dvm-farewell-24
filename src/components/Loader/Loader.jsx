e5c4660e5bb9e2d698de5b6bdd24df0a103cc4a2
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-04-20 06:04:46 +0530

    style: Modified preloader
    - Also disabled select for all items

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,15 @@ import * as styles from './Loader.module.scss'
 export default function Loader() {
     return (
         <main className={styles.loader}>
-            <h1>Loading</h1>
+            {/* <h1>Loading...</h1> */}
+            <div className={styles.wrapper}>
+                <div className={styles.circle}></div>
+                <div className={styles.circle}></div>
+                <div className={styles.circle}></div>
+                <div className={styles.shadow}></div>
+                <div className={styles.shadow}></div>
+                <div className={styles.shadow}></div>
+            </div>
         </main>
     )
 }

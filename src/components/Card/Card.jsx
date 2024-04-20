0de0b9417ac19dbbdcdd1c75450bf911dbf9c358
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-04-20 22:51:30 +0530

    style: reset

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,6 @@
 import * as styles from './Card.module.scss'
 
 export default function Card({ seniorCard, prevBtn, nextBtn, onPrev, onNext }) {
-    const htmlElement = document.querySelector('html')
-    let zoom = Math.floor(((window.outerWidth - 10) / window.innerWidth) * 100)
-
-    if (zoom <= 100) {
-        htmlElement.style.fontSize = '16px'
-    }
-    if (zoom > 100 && zoom <= 111) {
-        htmlElement.style.fontSize = '12px'
-    }
-    if (zoom > 111 && zoom <= 151) {
-        htmlElement.style.fontSize = '10px'
-    }
-    if (zoom > 151 && zoom <= 201) {
-        htmlElement.style.fontSize = '8px'
-    }
 
     return (
         <div className={styles.cardContainer}>
